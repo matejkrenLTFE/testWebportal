a029357bb553ddd0b85aaa382714295f7158bd21
Matej Kren <matej.kren@gmail.com>
2019-08-20 07:10:51 +0200

    eslint settings

--- webmng-dev/src/AppMain.js
+++ webmng-dev/src/AppMain.js
@@ -2,6 +2,8 @@
  * Main application component
  * @author LTFE
  */
+/* global console, document, AppMain, $, defined, dmp, window */
+/* jslint browser:true, node:true*/
 
 global.dmp = function (input) {
     "use strict";
@@ -14,9 +16,8 @@ global.dmp = function (input) {
  */
 global.defined = function (variable) {
     "use strict";
-    return (typeof variable !== "undefined" && variable !== null);
+    return variable !== undefined && variable !== null;
 };
-
 
 /**
  * Generate random string.
@@ -29,26 +30,19 @@ global.randomStr = function (len) {
 };
 
 /**
- * Create uptime from seconds.
- */
-global.uptimeFormat = function (seconds) {
-    "use strict";
-    const dayHours = Math.floor(seconds / (60 * 60));
-    const minutes = Math.floor(seconds % (60 * 60) / 60);
-    const days = Math.floor(dayHours / 24);
-    const hours = Math.floor((seconds - (days * 86400)) / 3600);
-
-    return days + " " + AppMain.t("DAYS", "global") + ", " + hours + " " + AppMain.t("HOURS", "global") + ", " + minutes + " " + AppMain.t("MINUTES", "global");
-};
-
-/**
  * Clear all cookies
  */
-global.clearCookies = function() {
+global.clearCookies = function () {
+    "use strict";
     const cookies = document.cookie.split(";");
     cookies.forEach(function (cookie) {
         const eqPos = cookie.indexOf("=");
-        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        let name;
+        if (eqPos > -1) {
+            name = cookie.substr(0, eqPos);
+        } else {
+            name = cookie;
+        }
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     });
 };
@@ -65,7 +59,8 @@ const modulerbac = require("./AppRBAC.js");
 const appMessages = require("./includes/appMessages.js");
 const X2JS = require("xml-json-parser");
 
-global.AppMain = function(conf) {
+global.AppMain = function (conf) {
+    "use strict";
     const config = conf || {};
 
     this.STATE_LOADED = 1;
@@ -88,17 +83,17 @@ global.AppMain = function(conf) {
     /**
      * AppView instance.
      */
-     this.view = null;
+    this.view = null;
 
     /**
      * AppHTML HTML rendering helper.
      */
-    this.html=null;
+    this.html = null;
 
     /**
      * AppController instance.
      */
-    this.controller=null;
+    this.controller = null;
 
     /**
      * AppWebserviceClient instance.
@@ -162,7 +157,7 @@ global.AppMain = function(conf) {
     /**
      * Trigger callback when AppMain state changes, @see STATE_* constants
      */
-    this.onStateChange=null;
+    this.onStateChange = null;
 
     /**
      * Application components which should be accessed globally.
@@ -170,14 +165,49 @@ global.AppMain = function(conf) {
     let _components = {};
 
     /**
+     * Set app configuration passed into constructor.
+     */
+    const _setConfig = function (_this, config) {
+        config.forEach(function (param) {
+            if (_this[param] !== undefined) {
+                _this[param] = config[param];
+            }
+        });
+    };
+
+    /**
+     * Init application routes.
+     */
+    function _initRoutes(_this) {
+        _routes = {
+            base: "/",
+            login: "/",
+            app: (_this.environment === _this.ENVIRONMENT_DEV)
+                ? "/webmng-dev"
+                : "/webmng"
+        };
+    }
+
+    /**
+     * Triggered when application state changes:
+     * - application was loaded (after AppMain.run)
+     * - controller action exected
+     */
+    const _stateChanged = function (_this, state) {
+        if (typeof _this.onStateChange === "function") {
+            _this.onStateChange({"state": state});
+        }
+    };
+
+    /**
      * Initialize application before AppMain.run()
      */
-    this.init = function() {
+    this.init = function () {
         dmp("AppMain.init");
         _setConfig(this, config);
         _initRoutes(this);
 
-        if (defined(this.afterInit) && (typeof this.afterInit==="function")) {
+        if (defined(this.afterInit) && (typeof this.afterInit === "function")) {
             this.afterInit(this);
         }
         // Init HTML helper
@@ -190,7 +220,7 @@ global.AppMain = function(conf) {
         this.locale = new modulelocale.AppLocale(this.getLanguage());
         this.locale.setSupportedLocale(this.supportedLocale);
     };
-    this.run = function() {
+    this.run = function () {
         this.init();
 
         // AuthFormLogoutLocation fix
@@ -200,20 +230,24 @@ global.AppMain = function(conf) {
             window.location = "/";
         }
         const loginUrl = this.getUrl("login") + "/";
-        this.controller=new modulecontroller.AppController();
-        this.controller.actionDefault = (window.location.href === loginUrl || window.location.search==="?logout") ? "Login" : "Default";
+        this.controller = new modulecontroller.AppController();
+        this.controller.actionDefault = (window.location.href === loginUrl || window.location.search === "?logout")
+            ? "Login"
+            : "Default";
 
         dmp("AppMain.run");
         dmp(this.controller.actionDefault);
 
         this.view = new moduleview.AppView();
         this.view.canvas = ".section.main-canvas";
-        this.view.replaceEmptyVars = (defined(config.replaceEmptyVars)) ? config.replaceEmptyVars : true;
+        this.view.replaceEmptyVars = (defined(config.replaceEmptyVars))
+            ? config.replaceEmptyVars
+            : true;
         this.view.controller = this.controller;
         this.controller.view = this.view;
 
         // user login because of certificate login
-        if(config.authType === AppMain.AUTH_TYPE_CERT){
+        if (config.authType === AppMain.AUTH_TYPE_CERT) {
             this.certificateInitUser();
         }
 
@@ -225,10 +259,10 @@ global.AppMain = function(conf) {
         this.bootstrap.init();
 
         this.logger.env = this.environment;
-        this.controller.exec( window.location.hash.substring(1) );
+        this.controller.exec(window.location.hash.substring(1));
 
         const _this = this;
-        $(window).bind("hashchange", function(e){
+        $(window).bind("hashchange", function (e) {
             $(".main-canvas").css("height", "unset");
             $("main.mdl-layout__content").removeClass("is-full-screen");
             _this.controller.exec(window.location.hash.substring(1), e);
@@ -245,7 +279,7 @@ global.AppMain = function(conf) {
             // Expand all
             const exMenu = $("#expandMenu");
             const colMenu = $("#collapseMenu");
-            exMenu.on("click", function(){
+            exMenu.on("click", function () {
                 $("ul.mdl-navigation__submenu-active").removeClass("mdl-navigation__submenu-active");
                 // Expand all submenus
                 $("ul.list-navigation--sublevel").addClass("mdl-navigation__submenu-active");
@@ -253,42 +287,22 @@ global.AppMain = function(conf) {
                 exMenu.hide();
             });
             // collapse all
-            colMenu.on("click", function(){
+            colMenu.on("click", function () {
                 $("ul.list-navigation--sublevel").removeClass("mdl-navigation__submenu-active");
                 colMenu.hide();
                 exMenu.show();
             });
-        },500);
+        }, 500);
     };
 
-    this.loggedIn = function() {
-        return (sessionStorage.getItem("loggedIn")===true);
+    this.loggedIn = function () {
+        return (sessionStorage.getItem("loggedIn") === true);
     };
 
-    this.getProtocol = function() {
-        return this.httpsEnabled ? "https://" : "http://";
-    };
-
-    /**
-     * Triggered when application state changes:
-     * - application was loaded (after AppMain.run)
-     * - controller action exected
-     */
-    const _stateChanged = function(_this, state) {
-        if (typeof _this.onStateChange === "function") {
-            _this.onStateChange({"state": state});
-        }
-    };
-
-    /**
-     * Set app configuration passed into constructor.
-     */
-    const _setConfig = function(_this, config) {
-        config.forEach(function (param) {
-            if (typeof _this[param] !== "undefined") {
-                _this[param] = config[param];
-            }
-        });
+    this.getProtocol = function () {
+        return this.httpsEnabled
+            ? "https://"
+            : "http://";
     };
 
     /**
@@ -296,7 +310,7 @@ global.AppMain = function(conf) {
      * @param {String} paramName Optional.
      * @return {mixed} App config object or config param value.
      */
-    this.getConfigParams = function(paramName) {
+    this.getConfigParams = function (paramName) {
         if (paramName) {
             if (defined(config[paramName])) {
                 return config[paramName];
@@ -313,24 +327,14 @@ global.AppMain = function(conf) {
      * - "login"
      * - "app"
      */
-    this.getUrl = function(routeName) {
-        if (typeof routeName === "undefined" || typeof _routes[routeName] === "undefined") {
+    this.getUrl = function (routeName) {
+        if (routeName === undefined || _routes[routeName] === undefined) {
             throw "getUrl: undefined or invalid route --> " + routeName;
         }
-
-        return (_routes[routeName]==="/") ? this.getProtocol() + window.location.hostname : this.getProtocol() + window.location.hostname + _routes[routeName];
+        return (_routes[routeName] === "/")
+            ? this.getProtocol() + window.location.hostname
+            : this.getProtocol() + window.location.hostname + _routes[routeName];
     };
-
-    /**
-     * Init application routes.
-     */
-    function _initRoutes(_this) {
-        _routes = {
-            base: "/",
-            login: "/",
-            app: (_this.environment===_this.ENVIRONMENT_DEV) ? "/webmng-dev" : "/webmng"
-        };
-    }
 
 
     // /**
@@ -344,9 +348,13 @@ global.AppMain = function(conf) {
     /**
      * Get current active language. Default: en_US
      */
-    this.getLanguage = function() {
+    this.getLanguage = function () {
         const locale = sessionStorage.getItem("locale");
-        return (locale) ? locale : "en_US";
+        if (locale) {
+            return locale;
+        } else {
+            return "en_US";
+        }
     };
 
     /**
@@ -356,7 +364,7 @@ global.AppMain = function(conf) {
      * @param stringVars Optional
      * @return {String} translate string.
      */
-    this.t = function(string, context, stringVars) {
+    this.t = function (string, context, stringVars) {
         return this.locale.stringTranslate(string, context, stringVars);
     };
 
@@ -365,8 +373,8 @@ global.AppMain = function(conf) {
      * @param {String} componentName Component name in lowercase without App prefix.
      * @return {Object} initialized component.
      */
-    this.getAppComponent = function(componentName) {
-        if(defined(this[componentName])) {
+    this.getAppComponent = function (componentName) {
+        if (defined(this[componentName])) {
             return this[componentName];
         }
         throw "AppMain: call to non-existing component " + componentName;
@@ -379,7 +387,7 @@ global.AppMain = function(conf) {
      * @param  vars.
      * @param {String} type Dialog type: default, warning, error, success. If no type is provided "default" is which disappears after 5sec.
      */
-    this.dialog = function(message, type, vars) {
+    this.dialog = function (message, type, vars) {
         this.view.showDialogMessage(AppMain.t(message, "dialog", vars), type);
     };
 
@@ -388,16 +396,18 @@ global.AppMain = function(conf) {
      * @see AppLocale.localeDefault.localization
      * @return {string}
      */
-    this.localization = function(paramName, selectLocale) {
+    this.localization = function (paramName, selectLocale) {
         const params = this.locale.localization(selectLocale);
-        return defined(params[paramName]) ? params[paramName] : params;
+        return defined(params[paramName])
+            ? params[paramName]
+            : params;
     };
 
     /**
      * Get AppWebserviceClient instance.
      * @return {AppWebserviceClient}
      */
-    this.ws = function() {
+    this.ws = function () {
         if (_webservice === null) {
             _webservice = new modulewebservice.AppWebserviceClient();
             _webservice.setNamespace("gw:");
@@ -409,7 +419,7 @@ global.AppMain = function(conf) {
      * Get AppWebserviceClient instance.
      * @return {AppWebserviceClient}
      */
-    this.wsMes = function() {
+    this.wsMes = function () {
         if (_webserviceMes === null) {
             _webserviceMes = new modulewebservice.AppWebserviceClient();
             _webserviceMes.setNamespace("mes:");
@@ -421,8 +431,8 @@ global.AppMain = function(conf) {
     /**
      * Set custom global component.
      */
-    this.setComponent = function(name, component) {
-        _components[name]=component;
+    this.setComponent = function (name, component) {
+        _components[name] = component;
     };
 
     /**
@@ -430,22 +440,24 @@ global.AppMain = function(conf) {
      *
      * @param {string} name Component name.
      */
-    this.getComponent = function(name) {
-        return defined(_components[name]) ? _components[name] : null;
+    this.getComponent = function (name) {
+        return defined(_components[name])
+            ? _components[name]
+            : null;
     };
 
     /**
      * Add log message to internal app logger.
      * @param {String} message
      */
-    this.log = function(message) {
+    this.log = function (message) {
         this.logger[this.logger.length] = message;
     };
 
     /**
      * Get internal app logger trace.
      */
-    this.getLogger = function() {
+    this.getLogger = function () {
         return this.logger;
     };
 
@@ -454,8 +466,10 @@ global.AppMain = function(conf) {
      * @param {String} messageId
      * @return {String}
      */
-    this.getAppMessage = function(messageId) {
-        return defined(appMessages[messageId]) ? this.t(appMessages[messageId], "global") : "";
+    this.getAppMessage = function (messageId) {
+        return defined(appMessages[messageId])
+            ? this.t(appMessages[messageId], "global")
+            : "";
     };
 
     this.certificateInitUser = function () {
@@ -466,18 +480,30 @@ global.AppMain = function(conf) {
 
         if (defined(userData.GetUserDataResponse) && defined(userData.GetUserDataResponse.user) && defined(userData.GetUserDataResponse.role)) {
             const user = userData.GetUserDataResponse.user;
-            user.role = Json2Xml.xml_str2json( "<role>" + userData.GetUserDataResponse.role["user-role"] + "</role>" );
-            if(user.role && user.role.role) {
+            user.role = Json2Xml.xml_str2json("<role>" + userData.GetUserDataResponse.role["user-role"] + "</role>");
+            if (user.role && user.role.role) {
                 user.role = user.role.role;
-            }
-            else {
+            } else {
                 return;
             }
             user["access-level"] = userData.GetUserDataResponse.role["access-level"];
             dmp("USER_DATA");
-            dmp( userData );
+            dmp(userData);
             AppMain.user.setUserData(user);
             // CtrlActionLogin.controller.userLogin(user);
         }
     };
+};
+
+/**
+ * Create uptime from seconds.
+ */
+global.uptimeFormat = function (seconds) {
+    "use strict";
+    const dayHours = Math.floor(seconds / (60 * 60));
+    const minutes = Math.floor(seconds % (60 * 60) / 60);
+    const days = Math.floor(dayHours / 24);
+    const hours = Math.floor((seconds - (days * 86400)) / 3600);
+
+    return days + " " + AppMain.t("DAYS", "global") + ", " + hours + " " + AppMain.t("HOURS", "global") + ", " + minutes + " " + AppMain.t("MINUTES", "global");
 };
