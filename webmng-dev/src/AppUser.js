14a971821b1347acafc875fd1f925e7b8f1eef87
Matej Kren <matej.kren@gmail.com>
2019-08-26 19:30:16 +0200

    AppUser code review first fix

--- webmng-dev/src/AppUser.js
+++ webmng-dev/src/AppUser.js
@@ -2,8 +2,15 @@
  * Component for application user session.
  * @author LTFE
  */
-module.exports.AppUser = function() 
-{
+
+/* global  AppMain, defined, window */
+/* jshint maxstatements: false */
+/* jslint browser:true, node:true*/
+/* eslint es6:0, no-undefined:0, control-has-associated-label:0  */
+
+module.exports.AppUser = function () {
+    "use strict";
+
     /**
      * Cached user data.
      * @return  {Object}
@@ -13,36 +20,40 @@ module.exports.AppUser = function()
     /**
      * Get user data.
      */
-    this.getUserData = function(name) {
-        if (_userData===null)
+    this.getUserData = function (name) {
+        if (_userData === null) {
             _userData = JSON.parse(localStorage.getItem("userData"));
-        
-        if (_userData===null)
+        }
+        if (_userData === null) {
             return null;
+        }
 
-        if (!defined(name))
+        if (!defined(name)) {
             return _userData;
+        }
 
-        return defined(_userData[name]) ? _userData[name] : null;
+        return defined(_userData[name])
+            ? _userData[name]
+            : null;
     };
 
     /**
      * Set user session data object.
      */
-    this.setUserData = function(data) {
-        localStorage.setItem("userData", JSON.stringify(data) );
+    this.setUserData = function (data) {
+        localStorage.setItem("userData", JSON.stringify(data));
     };
 
     /**
      * Check if user is logged-in.
      * @return {Boolean}
      */
-    this.loggedIn = function() {
+    this.loggedIn = function () {
         return !!this.getUserData("username");
     };
 
-    this.logout = function() {
-        localStorage.clear();        
+    this.logout = function () {
+        localStorage.clear();
         sessionStorage.clear();
         window.location = AppMain.getUrl("base") + "/logout";
     };
@@ -50,16 +61,16 @@ module.exports.AppUser = function()
     /**
      * Get RBAC map for current user.
      */
-    this.getRBACMap = function() {
+    this.getRBACMap = function () {
         return AppMain.user.getUserData("role");
     };
 
     /**
      * Get RBAC permission for current user for given category and param.
      */
-    this.getRBACpermissionElement = function(category, param) {
+    this.getRBACpermissionElement = function (category, param) {
         const rbac = this.getRBACMap();
         return !(defined(rbac[category]) && defined(rbac[category][param])
-            && (rbac[category][param].toLowerCase() === "h" || rbac[category][param].toLowerCase() === "r"));
-    }
+                && (rbac[category][param].toLowerCase() === "h" || rbac[category][param].toLowerCase() === "r"));
+    };
 };
