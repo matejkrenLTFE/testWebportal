66b7e2e60352c2e5fb9649e7fde5789203ad0fe3
Matej Kren <matej.kren@gmail.com>
2019-08-28 07:28:08 +0200

    security/detect-non-literal-regexp fix test

--- webmng-dev/src/AppController.js
+++ webmng-dev/src/AppController.js
@@ -125,7 +125,7 @@ module.exports.AppController = function () {
     function getControllerActionComponent(action) {
         let controlleractioncomponent = null;
         if (controllers.indexOf(action) !== -1) { // protect dynamic require
-            controlleractioncomponent = require("./CtrlAction" + `${action}`);// eslint-disable-line security/detect-non-literal-require
+            controlleractioncomponent = require("./CtrlAction" + action);// eslint-disable-line security/detect-non-literal-require
         } else {
             AppMain.log("Module not found.");
             dmp("AppMain.exec controller action exception message: Module not found.");
